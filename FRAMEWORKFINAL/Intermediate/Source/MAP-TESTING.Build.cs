602570bfa5f1dad7fa090cb59c8191fc614eac3b
ThatGuyOmni <shaun.broderick@outlook.com>
2024-01-28 16:56:27 +0000

    FINAL

---
+++ FRAMEWORKFINAL/Intermediate/Source/MAP-TESTING.Build.cs
@@ -0,0 +1,12 @@
+using UnrealBuildTool;
+
+public class MAP-TESTING : ModuleRules
+{
+	public MAP-TESTING(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		PrivateDependencyModuleNames.Add("Core");
+		PrivateDependencyModuleNames.Add("Core");
+	}
+}
